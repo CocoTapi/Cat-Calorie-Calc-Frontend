47d04dd9edaf2755776571b53402a61cada6149b
CocoTapi <shioriavino@gmail.com>
2025-03-11 17:41:51 -0400

    add form html

--- src/app/pet-profile/pet-form/pet-form.component.ts
+++ src/app/pet-profile/pet-form/pet-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../ui/card/card.component";
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-pet-form',
-  imports: [CardComponent],
+  imports: [CardComponent, ReactiveFormsModule],
   templateUrl: './pet-form.component.html',
   styleUrl: './pet-form.component.scss'
 })
@@ -11,5 +13,8 @@ export class PetFormComponent {
   // TODO: get the page info, edit or new
   title = signal('Edit Profile');
 
-
+  
+  onSubmit() {
+    console.log("Submitted")
+  }
 }
