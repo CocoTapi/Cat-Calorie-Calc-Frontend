a51cce738510e517c6060110533f948520fc85d6
CocoTapi <shioriavino@gmail.com>
2025-03-24 23:30:45 -0400

    fix to handle the undefined case

--- src/app/pet-profile/pet-profile.component.ts
+++ src/app/pet-profile/pet-profile.component.ts
@@ -7,23 +7,40 @@ import { SlidePanelComponent } from '../ui/slide-panel/slide-panel.component';
 import { CardComponent } from '../ui/card/card.component';
 import { CommonConstants } from '../app.constants';
 import { FormGroup } from '@angular/forms';
-import { Pet_Form_Data } from './models/pet-profile.model';
+import { Pet_Form_Data, Pet_Profile } from './models/pet-profile.model';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { catchError, of } from 'rxjs';
 
 
 @Component({
   selector: 'app-pet-profile',
- imports: [PetFormComponent, MatIconModule, SlidePanelComponent, CardComponent],
+  imports: [PetFormComponent, MatIconModule, SlidePanelComponent, CardComponent],
   templateUrl: './pet-profile.component.html',
   styleUrl: './pet-profile.component.scss'
 })
 export class PetProfileComponent implements AfterViewInit {
   id = input<number>(0);
-  panelId: string = CommonConstants.PET_FORM;
-
   private petProfileService = inject(PetProfileService);
   private slidePanelService = inject(SlidePanelService);
+  panelId: string = CommonConstants.PET_FORM;
+
+  private _pet = toSignal<Pet_Profile | undefined>(
+    this.petProfileService.getPetById(this.id()).pipe(
+      catchError((error) => {
+        console.error('Error fetching Pet_Profile:', error);
+        return of(undefined);
+      })
+    ),
+    { initialValue: undefined }
+  );
+
+  readonly pet = computed(() => {
+    const pet = this._pet();
+    if (!pet) throw new Error('Pet not loaded yet or failed to load.');
+    return pet;
+  });
+
+
 
   formValid: boolean = false;
   petFormGroup!: FormGroup;
@@ -33,21 +50,7 @@ export class PetProfileComponent implements AfterViewInit {
     this.slidePanelService.canClose(this.panelId, () => this.canPanelClose());
   }
 
-
   showEditPage = signal(false);
-
-  // handle Observable as signal
-  pet = toSignal(
-    this.petProfileService.getPetById(this.id()).pipe(
-      catchError((error) => {
-        console.error('Error fetching pet:', error);
-        return of(undefined); // fallback so toSignal doesn’t break
-      })
-    ), 
-    {
-      initialValue: undefined,
-    }
-  );
 
   imagePath = computed(() => {
     const icon = 'pets/' + this.pet()?.icon;
@@ -56,7 +59,7 @@ export class PetProfileComponent implements AfterViewInit {
 
   age = computed(() => {
     const birthday = this.pet()?.birthday;
-    if(!birthday) return 0;
+    if (!birthday) return 0;
 
     const today = new Date();
     let age = today.getFullYear() - birthday!.getFullYear();
@@ -64,7 +67,7 @@ export class PetProfileComponent implements AfterViewInit {
 
     // Adjust age if birthday hasn't occurred this year yet
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthday!.getDate())) {
-      age--; 
+      age--;
     }
 
     return age;
@@ -72,6 +75,9 @@ export class PetProfileComponent implements AfterViewInit {
 
   graphTitle = computed(() => {
     let title = `Goal: ${this.pet()?.goal} weight`;
+    if (!title) {
+      return ''
+    }
 
     if (this.pet()?.target_weight) {
       title = `Goal: ${this.pet()?.goal} Weight to ${this.pet()?.target_weight} ${this.pet()?.weight_unit}`
@@ -85,7 +91,7 @@ export class PetProfileComponent implements AfterViewInit {
   onFormStatusChanged(valid: boolean) {
     this.formValid = valid;
   }
-  
+
   onFormGroupChanged(form: FormGroup) {
     this.petFormGroup = form;
   }
@@ -105,8 +111,8 @@ export class PetProfileComponent implements AfterViewInit {
 
     // Submit form
     const formData: Pet_Form_Data = {
-          id: this.id(),
-          ...this.petFormGroup.value
+      id: this.id(),
+      ...this.petFormGroup.value
     }
 
     if (!formData.allergies.length) {
@@ -116,5 +122,5 @@ export class PetProfileComponent implements AfterViewInit {
     console.log('form submitted by closing the panel', formData);
     return true;
   }
-  
+
 }
